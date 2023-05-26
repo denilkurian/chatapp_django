f0103b957fff73eb1a07522ac68d1297c6346a65
denil kurian <denilkuriancc@gmail.com>
2023-05-26 23:36:58 +0530

    init

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -94,25 +94,25 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "chatdb",
-        'USER' : 'root',
-        'PASSWORD' : 'denil_kurian@123',
-        'HOST' : 'localhost',
-        'PORT' : '3306'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': "chatdb",
+#         'USER' : 'root',
+#         'PASSWORD' : 'denil_kurian@123',
+#         'HOST' : 'localhost',
+#         'PORT' : '3306'
+#
+#     }
+# }
 
 
 
